47822be9758a7cf576c8af438a91b5a652523818
zhubs <qzmpbaoshi@163.com>
2019-05-28 11:27:44 +0800

    更新NewJson

--- YDVS/PlatformServer/WebAppService/FileAdapter/IAdapter/IFileAdapter.cs
+++ YDVS/PlatformServer/WebAppService/FileAdapter/IAdapter/IFileAdapter.cs
@@ -13,8 +13,9 @@ namespace WebAppService.FileAdapter
         /// 获取子文件或文件夹
         /// </summary>
         /// <param name="path">相对文件夹路径</param>
-        /// <param name="path">是否获取文件节点，默认为flase只获取文件夹</param>
+        /// <param name="isGetDic">是否获取文件夹节点，默认为flase</param>
+        /// <param name="isGetFile">是否获取文件节点，默认为flase</param>
         /// <returns></returns>
-        List<FileTreeItemModel> GetFileTreeList(string path, bool isGetFile = false);
+        List<FileTreeItemModel> GetFileTreeList(string path, bool isGetDic = true, bool isGetFile = false);
     }
 }
