b40601334c2b2161ded7939732fd0f2cbf662317
zhubs <qzmpbaoshi@163.com>
2019-06-13 10:50:12 +0800

    修改输出结果model

---
+++ YDVS/SystemModel/RequestResult/RequestResult.cs
@@ -0,0 +1,67 @@
+﻿namespace SystemModel.RequestResult
+{
+    public class RequestResult<T>
+    {
+
+        private int _totalCount;
+        private T _resultData;
+        private bool _flag;
+        private string _msg;
+        /// <summary>
+        /// 数据总数
+        /// </summary>
+        public int TotalCount
+        {
+            get
+            {
+                return _totalCount;
+            }
+
+            set
+            {
+                _totalCount = value;
+            }
+        }
+        /// <summary>
+        /// 结果集
+        /// </summary>
+        public T ResultData
+        {
+            get
+            {
+                return _resultData;
+            }
+
+            set
+            {
+                _resultData = value;
+            }
+        }
+
+        public bool Flag
+        {
+            get
+            {
+                return _flag;
+            }
+
+            set
+            {
+                _flag = value;
+            }
+        }
+
+        public string Msg
+        {
+            get
+            {
+                return _msg;
+            }
+
+            set
+            {
+                _msg = value;
+            }
+        }
+    }
+}
