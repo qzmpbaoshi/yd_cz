b2c82211a185d097d98780c59bf26c24b3b52381
zhubs <qzmpbaoshi@163.com>
2019-06-13 10:43:34 +0800

    提交更改

---
+++ YDVS/SystemModel/DataModel/DelAnalysisReportInfoModel.cs
@@ -0,0 +1,8 @@
+﻿namespace SystemModel.DataModel
+{
+    public class DelAnalysisReportInfoModel
+    {
+        public string AnalysisReportInfoId { get; set; }
+        public string RelativePath { get; set; }
+    }
+}
