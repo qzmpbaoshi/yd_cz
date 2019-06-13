3ec36425833b61cd13658b7728ec73c5243cd9b1
zhubs <qzmpbaoshi@163.com>
2019-06-13 11:04:20 +0800

    提交缺失的文件

---
+++ YDVS/PlatformServer/WebAppService/Controllers/AnalysisReportInfoController.cs
@@ -0,0 +1,49 @@
+﻿using System.IO;
+using System.Web.Http;
+using SystemModel.DataModel;
+using SystemModel.RequestResult;
+using WebAppService.DataAdapter;
+
+namespace WebAppService.Controllers
+{
+    public class AnalysisReportInfoController : ApiController
+    {
+        private IAnalysisReportInfoAdapter DBAdapter { get; set; }
+        public AnalysisReportInfoController()
+        {
+            this.DBAdapter = new AnalysisReportInfoAdapter();
+        }
+        /// <summary>
+        /// 添加方法
+        /// </summary>
+        /// <param name="addModel"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public IHttpActionResult AddAnalysisReportInfo([FromBody]AnalysisReportInfoModel addModel)
+        {
+            RequestResult<string> rst = this.DBAdapter.AddAnalysisReportInfo(addModel);
+            return Json<RequestResult<string>>(rst);
+        }
+
+        /// <summary>
+        /// 删除方法
+        /// </summary>
+        /// <param name="delId"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public IHttpActionResult DelAnalysisReportInfo([FromBody]DelAnalysisReportInfoModel delModel)
+        {
+            RequestEasyResult rst = new RequestEasyResult();
+            if (!string.IsNullOrWhiteSpace(delModel.AnalysisReportInfoId))
+                rst = this.DBAdapter.DelAnalysisReportInfosById(delModel.AnalysisReportInfoId);
+            if (string.IsNullOrWhiteSpace(delModel.RelativePath)) return Json<RequestEasyResult>(rst); ;
+            string dirPath = ConfigerHelper.GetBaseAttPath() + delModel.RelativePath;
+            if (!string.IsNullOrWhiteSpace(dirPath))
+            {
+                if (Directory.Exists(dirPath))
+                    Directory.Delete(dirPath, true);
+            }
+            return Json<RequestEasyResult>(rst);
+        }
+    }
+}
